23151185d2d332250e15fe81ab2107f29b526da7
mdahumeniuk@tjsp.jus.br <marcelo.humeniuk@mps.com.br>
2021-09-03 19:53:23 +0000

    ajuste

--- src/CQRS.Services.CustomerWorker/Program.cs
+++ src/CQRS.Services.CustomerWorker/Program.cs
@@ -2,7 +2,6 @@ using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.IO;
 
 namespace CQRS.Services.CustomerWorker
 {
@@ -13,31 +12,21 @@ namespace CQRS.Services.CustomerWorker
             CreateHostBuilder(args).Build().Run();
         }
 
-
-        private static IConfiguration Configuration { get; set; }
-
         public static IHostBuilder CreateHostBuilder(string[] args) =>
 
         Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration(AppConfiguration)
+
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<Worker>();
 
                     services.AddAzureClients(builder =>
                     {
-                        builder.AddServiceBusClient(Configuration.GetConnectionString("ServiceBus"));
+                        builder.AddServiceBusClient(hostContext.Configuration.GetConnectionString("ServiceBus"));
                     });
+
+
                 });
-
-        //TODO: VOLTAR E VERIFICAR VARIAVEL DE AMBIENTE
-        private static void AppConfiguration(IConfigurationBuilder config)
-        {
-            config.SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("AppSettings.Development.json");
-
-            Configuration = config.Build();
-        }
     }
 }
 
